61c714c510b032f04b5882e5158858a53786106c
DineshGowthamK <dineshgowtham33@gmail.com>
2024-01-09 11:29:35 +0530

    Edited

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-import store from './redux/Store.jsx';
-import {Provider} from 'react-redux';
+import store from './redux/store.jsx'
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
-    <ToastContainer autoClose={2000}/>
+      <ToastContainer autoClose={2000}/>
   </React.StrictMode>,
 )
